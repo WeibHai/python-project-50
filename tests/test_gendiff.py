ff8ab62201790fdf2d5bb203bd983d6c01c0c7f3
марк проскурин <proskurina-1956@mail.ru>
2022-12-12 21:53:45 +0300

    fix bugs

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,34 +3,28 @@ from gendiff.difference_finder import get_diff
 from gendiff import generate_diff
 from tests.fixtures import expected_results
 import json
+import pytest
 
 
-def test_file_reader():
-    assert get_data('tests/fixtures/file1.json') == expected_results.data_file1
-    assert get_data('tests/fixtures/file2.yml') == expected_results.data_file2
-    assert get_data('tests/fixtures/r_file1.json') == expected_results.data_r_file1
-    assert get_data('tests/fixtures/r_file2.yml') == expected_results.data_r_file2
+@pytest.mark.parametrize("path, expected", [
+    ('tests/fixtures/file1.json', 'tests/fixtures/data1.txt'), 
+    ('tests/fixtures/file2.json', 'tests/fixtures/data2.txt'),
+    ('tests/fixtures/file1.yml', 'tests/fixtures/data1.txt'),
+    ('tests/fixtures/file2.yml', 'tests/fixtures/data2.txt')])
+def test_file_reader(path, expected):
+    with open(expected, 'r') as file:
+        result = file.read()
+    assert str(get_data(path)) == result
 
 
-def test_difference_finder():
-    assert get_diff(get_data('tests/fixtures/file1.json'), get_data('tests/fixtures/file2.json')) == expected_results.internal_representation
-    assert get_diff(get_data('tests/fixtures/file1.yml'), get_data('tests/fixtures/file2.yml')) == expected_results.internal_representation
-    assert get_diff(get_data('tests/fixtures/r_file1.json'), get_data('tests/fixtures/r_file2.json')) == expected_results.r_internal_representation
-    assert get_diff(get_data('tests/fixtures/r_file1.yml'), get_data('tests/fixtures/r_file2.yml')) == expected_results.r_internal_representation
-
-
-def test_generate_diff():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == expected_results.result_stylish
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == expected_results.result_stylish
-    assert generate_diff('tests/fixtures/r_file1.json', 'tests/fixtures/r_file2.json') == expected_results.result_stylish_r
-    assert generate_diff('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.yml') == expected_results.result_stylish_r
-
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', formatter='plain') == expected_results.result_plain
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', formatter='plain') == expected_results.result_plain
-    assert generate_diff('tests/fixtures/r_file1.json', 'tests/fixtures/r_file2.json', formatter='plain') == expected_results.result_plain_r
-    assert generate_diff('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.yml', formatter='plain') == expected_results.result_plain_r
-
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', formatter='json') == json.dumps(expected_results.internal_representation, indent=4)
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', formatter='json') == json.dumps(expected_results.internal_representation, indent=4)
-    assert generate_diff('tests/fixtures/r_file1.json', 'tests/fixtures/r_file2.json', formatter='json') == json.dumps(expected_results.r_internal_representation, indent=4)
-    assert generate_diff('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.yml', formatter='json') == json.dumps(expected_results.r_internal_representation, indent=4)
+@pytest.mark.parametrize("path, path2, formatter, expected", [
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'stylish', 'tests/fixtures/res_stylish.txt'), 
+    ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'stylish', 'tests/fixtures/res_rec_stylish.txt'),
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'plain', 'tests/fixtures/res_plain.txt'), 
+    ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'plain', 'tests/fixtures/res_rec_plain.txt'),
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'json', 'tests/fixtures/inter_represent.txt'), 
+    ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'json', 'tests/fixtures/inter_represent_r.txt')])
+def test_generate_diff(path, path2, formatter, expected):
+    with open(expected, 'r') as file:
+        result = file.read()
+    assert generate_diff(path, path2, formatter) == result
