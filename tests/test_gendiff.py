6090f2af273cc27802860b013af5ebae36a96801
марк проскурин <proskurina-1956@mail.ru>
2022-12-12 22:12:54 +0300

    fix bugs

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,10 @@
 from gendiff.file_reader import get_data
-from gendiff.difference_finder import get_diff
 from gendiff import generate_diff
-from tests.fixtures import expected_results
-import json
 import pytest
 
 
 @pytest.mark.parametrize("path, expected", [
-    ('tests/fixtures/file1.json', 'tests/fixtures/data1.txt'), 
+    ('tests/fixtures/file1.json', 'tests/fixtures/data1.txt'),
     ('tests/fixtures/file2.json', 'tests/fixtures/data2.txt'),
     ('tests/fixtures/file1.yml', 'tests/fixtures/data1.txt'),
     ('tests/fixtures/file2.yml', 'tests/fixtures/data2.txt')])
@@ -18,11 +15,11 @@ def test_file_reader(path, expected):
 
 
 @pytest.mark.parametrize("path, path2, formatter, expected", [
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'stylish', 'tests/fixtures/res_stylish.txt'), 
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'stylish', 'tests/fixtures/res_stylish.txt'),
     ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'stylish', 'tests/fixtures/res_rec_stylish.txt'),
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'plain', 'tests/fixtures/res_plain.txt'), 
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'plain', 'tests/fixtures/res_plain.txt'),
     ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'plain', 'tests/fixtures/res_rec_plain.txt'),
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'json', 'tests/fixtures/inter_represent.txt'), 
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml', 'json', 'tests/fixtures/inter_represent.txt'),
     ('tests/fixtures/r_file1.yml', 'tests/fixtures/r_file2.json', 'json', 'tests/fixtures/inter_represent_r.txt')])
 def test_generate_diff(path, path2, formatter, expected):
     with open(expected, 'r') as file:
