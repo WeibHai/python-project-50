306de44ff97f6bb0f4bfcc1c8c08cd9f5d31f3e9
марк проскурин <proskurina-1956@mail.ru>
2022-12-17 17:39:35 +0300

    fix bugs

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,9 @@ import argparse
 from gendiff import generate_diff
 
 
+#  project entry point
+
+
 parser = argparse.ArgumentParser(description='''
 Compares two configuration files and shows a difference.''')
 
